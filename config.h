e748f9fc33255b5ecc0e72961138ec04aa6fe26d
gabel <gabriel@nagelmichaelsen.de>
2022-05-27 16:43:55 +0200

    change colorscheme

--- config.h
+++ config.h
@@ -10,8 +10,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	               /*    fg         bg   */
 	[SchemeNorm] = { "#c8c093", "#1f1f28" },
-	[SchemeSel]  = { "#1f1f28", "#dcd7ba" },
-	[SchemeOut]  = { "#000000", "#00ffff" },
+	[SchemeSel]  = { "#7e9cd8", "#1f1f28" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
